e790e97a79c4366f4bb5e6c60764c4d59c3d3b2f
Hesham <elnahhas@gmail.com>
2015-07-07 16:16:30 -0400

    Fix

--- spec/parsehub/configuration_spec.rb
+++ spec/parsehub/configuration_spec.rb
@@ -13,4 +13,16 @@ describe Parsehub::Configuration do
   it "sets default api url" do
     Parsehub.configuration.host.must_equal("https://www.parsehub.com/api/v2")
   end
+
+  it "default debug to false" do
+    Parsehub.configuration.debug.must_equal(false)
+  end
+
+  it "sets debug option" do
+    Parsehub.configure do |config|
+      config.debug = true
+    end
+
+    Parsehub.configuration.debug.must_equal true
+  end
 end
